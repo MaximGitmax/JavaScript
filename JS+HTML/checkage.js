de2d4d74cdc8e10dc0bded08ba4f386ff8ad4880
Maxim Koreshkov <3050393@gmail.com>
2022-06-27 21:06:28 +0300

    Added JS+HTML

---
+++ JS+HTML/checkage.js
@@ -0,0 +1,22 @@
+
+    age_1 = prompt("How old are you?");
+    
+    if (age_1 === 0){
+        age_1 = age_1.toString();   //if 0 => "0"
+    }
+    if (typeof(age_1) == "string"){
+        age_1 = age_1.trim();           //if "    " => ""
+    }
+    if (!isNaN(age_1) && age_1 ){
+        age_2 = 18;
+        age_3 = 60;
+        if (age_1 < age_2){
+            document.getElementById("h2").innerHTML = `You don't have access cause your age is ${age_1} It's less then required`;
+        } else if (age_1 >= age_2 && age_1 < age_3) {
+            document.getElementById("h2").innerHTML = `Welcome  !`;
+        } else if (age_1 >= age_3){
+            document.getElementById("h2").innerHTML = `Keep calm and look Culture channel`;
+        } 
+    } else {
+        document.getElementById("h2").innerHTML = `Error`;
+    }
